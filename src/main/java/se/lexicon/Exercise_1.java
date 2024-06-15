1687d00b02ffb3c8086b0ec55b28441ba7924cff
Mohammed Dawood <m.dawood.780909@gmail.com>
2024-06-15 19:53:58 +0200

    Exercise_1 done

---
+++ src/main/java/se/lexicon/Exercise_1.java
@@ -0,0 +1,14 @@
+package se.lexicon;
+
+import java.time.LocalDate;
+
+public class Exercise_1 {
+
+    public static void exercise_1() {
+        // Create a LocalDate object of the current day
+        LocalDate currentDate = LocalDate.now();
+
+        // Print out the current date
+        System.out.println("Current date: " + currentDate);
+    }
+}
