f5ec81e75aac95a53b97894fafdcd8dde9b4a57c
Mohammed Dawood <m.dawood.780909@gmail.com>
2024-06-18 19:06:20 +0300

    Exercise_13 done

--- src/main/java/se/lexicon/App.java
+++ src/main/java/se/lexicon/App.java
@@ -37,10 +37,10 @@ public class App {
         // Exercise_11.exercise_11();
 
         // To call Exercise_12
-        Exercise_12.exercise_12();
+        // Exercise_12.exercise_12();
 
-        // To call Exercise_8
-        // Exercise_8.exercise_8();
+        // To call Exercise_13
+        Exercise_13.exercise_13();
 
         // To call Exercise_8
         // Exercise_8.exercise_8();
--- src/main/java/se/lexicon/Exercise_13.java
+++ src/main/java/se/lexicon/Exercise_13.java
@@ -1,4 +1,21 @@
 package se.lexicon;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Exercise_13 {
+
+    public static void exercise_13() {
+        // Create a LocalDateTime object with current date and time
+        LocalDateTime currentDateTime = LocalDateTime.now();
+
+        // Create a DateTimeFormatter with the desired date and time formats
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd 'and time' HH:mm");
+
+        // Format the current LocalDateTime using the formatter
+        String formattedDateTime = currentDateTime.format(formatter);
+
+        // Print out the formatted LocalDateTime
+        System.out.println("Formatted LocalDateTime: " + formattedDateTime);
+    }
 }
